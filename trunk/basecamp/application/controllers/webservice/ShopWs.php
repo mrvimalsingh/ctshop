39d2ce4640bdd71c37c2095b49ccea87bb55bad2
admin@codetwisters.ro <admin@codetwisters.ro@54d2a80f-631a-96df-9b89-f4aca558e293>
2011-06-30 23:35:18 +0000

    moved jsonrpc to its own class and implemented a basic structure for the CT webservices, also fixed the activerecord library

--- trunk/basecamp/application/controllers/webservice/ShopWs.php
+++ trunk/basecamp/application/controllers/webservice/ShopWs.php
@@ -10,51 +10,41 @@
 // TODO this might be refactored (maybe extract the WS part and make it a library so other controllers might implement other webservices and/or split WS in multiple classes that handle different parts of the backend)
 class ShopWs extends Controller {
 
-    var $error;
+    var $errorCode = 0;
+    var $errorMessage = "";
 
     function ShopWs() {
         parent::Controller();
     }
 
     function index() {
-        global $HTTP_RAW_POST_DATA;
-        // parse json input
-        $input = json_decode($HTTP_RAW_POST_DATA);
 
-        $response = array("jsonrpc" => "2.0");
-
-        if ($input !== null && is_object($input)) {
-            // check for required parameters
-            if (isset($input->method)) {
-                // this is where we call the method...
-                $response["result"] = $this->_callJsonRpcMethod($input->method,
-                                                                isset($input->params)?$input->params:null);
-                if ($response["result"] == null) {
-                    $response["error"] = $this->error;
-                }
-            } else {
-                // we can live without the method, but not the param
-                $response["error"] = array("code" => "-1001", "message" => "JSON-RPC Error: no method");
-            }
-            if (isset($input->id)) $response["id"] = $input->id;
-        } else {
-            // json rpc error
-            $response["error"] = array("code" => "-1000", "message" => "JSON could not be parsed");
-            $response["id"] = null;
-        }
-
-        header("Content-type:application/json");
-        echo json_encode($response);
     }
 
-    function _callJsonRpcMethod($method, $params) {
-        // for now we implement this
-        if (method_exists($this, $method)) {
-            return $this->$method($params);
+    function jsonrpc($ws = null) {
+
+        $webservices = array(
+            "products" => "WsProducts",
+            "general" => "WsGeneral",
+            "user" => "WsUser",
+            "orders" => "WsOrders"
+        );
+
+        $this->load->library('JsonRpc');
+        if ($ws == null) {
+            $this->jsonrpc->registerHandlerClass($this);
         } else {
-            $this->error = array("code" => "-1002", "message" => "JSON-RPC Error: method does not exist");
-            return null;
+            if ($webservices[$ws]) {
+                $handlerClassName = $webservices[$ws];
+                $handlerClassNameLc = strtolower($handlerClassName);
+                $this->load->library($handlerClassName);
+                $this->jsonrpc->registerHandlerClass($this->$handlerClassNameLc);
+            } else {
+                echo "Unknown webservice: ".$ws;
+                return;
+            }
         }
+        $this->jsonrpc->startJsonRpc();
     }
 
     function get_languages($params) {
@@ -66,7 +56,16 @@ class ShopWs extends Controller {
         return $result;
     }
 
-    function _createArrayFromModel($model, $params) {
+    function subtract($params) {
+        if (is_array($params) && count($params) == 2) {
+            return $params[0]-$params[1];
+        } else {
+            JsonRpc::setInvalidParamsError($this);
+            return null;
+        }
+    }
+
+    static function _createArrayFromModel($model, $params) {
         $res = array();
         foreach ($params as $p) {
             $res[$p] = $model->$p;
---
+++ trunk/basecamp/application/libraries/JsonRpc.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: codetwister
+ * Date: 7/1/11
+ * Time: 12:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+class JsonRpc {
+
+    var $classes = array();
+    var $error;
+
+
+    /**
+     * classes that register must have $errorCode $errorMessage variables defined
+     * @param  $class
+     * @return void
+     */
+    function registerHandlerClass($class) {
+        $this->classes[] = $class;
+    }
+
+    function startJsonRpc() {
+        global $HTTP_RAW_POST_DATA;
+        // parse json input
+        $input = json_decode($HTTP_RAW_POST_DATA);
+
+        if ($input !== null && (is_object($input) || is_array($input))) {
+            if (is_array($input)) {
+                $response = array();
+                foreach ($input as $i) {
+                    $response[] = $this->_processRequest($i);
+                }
+            } else {
+                $response = $this->_processRequest($input);
+            }
+        } else {
+            // json rpc error
+            $response = array("jsonrpc" => "2.0");
+            $response["error"] = array("code" => "-32700", "message" => "Parse error.");
+            $response["id"] = null;
+        }
+
+        header("Content-type:application/json");
+        echo json_encode($response);
+    }
+
+    private function _processRequest($jsonRpc) {
+        // initialize default values
+        $response = array("jsonrpc" => "2.0");
+        $response["result"] = null;
+        $response["error"] = null;
+        if ($this->_checkRequest($jsonRpc)) {
+            $this->_callJsonRpcMethod($jsonRpc, $response);
+        }
+        return $response;
+    }
+
+    private function _checkRequest($jsonRpc) {
+        if (!isset($jsonRpc->method)) {
+            $response["error"] = array("code" => "-32600", "message" => "Invalid Request.");
+            return false;
+        }
+        return true;
+    }
+
+    private function _callJsonRpcMethod($jsonRpc, &$response) {
+        if (isset($jsonRpc->id)) $response["id"] = $jsonRpc->id;
+
+        foreach ($this->classes as $cls) {
+            if (method_exists($cls, $jsonRpc->method)) {
+                $method = $jsonRpc->method;
+                $response["result"] = $cls->$method(isset($jsonRpc->params)?$jsonRpc->params:null);
+                $response["error"] = ($cls->errorCode < 0)?array("code" => $cls->errorCode, "message" => $cls->errorMessage):null;
+                return;
+            }
+        }
+
+        $response["error"] = array("code" => "-32601", "message" => "Method not found.");
+    }
+
+    public static function setInvalidParamsError($cls) {
+        $cls->errorCode = -32602;
+        $cls->errorMessage = "Invalid params.";
+    }
+
+}
